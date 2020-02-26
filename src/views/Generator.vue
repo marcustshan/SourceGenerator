baf8e9a6c284a3c10e32b87870338400052d6863
tshan <hts0215@naver.com>
2020-02-26 18:33:41 +0900

    기능 수정 중

--- src/views/Generator.vue
+++ src/views/Generator.vue
@@ -13,33 +13,37 @@
           </li>
           <li class="input_item">
             <label class="input_label">패키지 명 ex) com.dm.test</label>
-            <input type="text" />
+            <input type="text" v-model="packageName" />
           </li>
           <li class="input_item">
             <label class="input_label">모듈 명</label>
-            <input type="text" />
+            <input type="text" v-model="moduleName" />
           </li>
           <li class="input_item">
             <label class="input_label">Controller 패키지 명</label>
-            <input type="text" />
+            <input type="text" v-model="controllerPackageName" />
           </li>
           <li class="input_item">
             <label class="input_label">Service 패키지 명</label>
-            <input type="text" />
+            <input type="text" v-model="servicePackageName" />
           </li>
           <li class="input_item">
             <label class="input_label">Model(DTO) 패키지 명</label>
-            <input type="text" />
+            <input type="text" v-model="modelPackageName" />
           </li>
           <li class="input_item">
             <label class="input_label">DAO 패키지 명</label>
-            <input type="text" />
+            <input type="text" v-model="daoPackageName" />
           </li>
           <li class="input_item">
             <label class="input_label">ServiceImpl 패키지 별도</label>
-            <input type="checkbox" />
+            <input type="checkbox" v-model="eachPackage" />
           </li>
         </ul>
+
+        <button @click="fnGenerate">
+          생성
+        </button>
       </div>
     </div>
     <div class="center_container">
@@ -55,10 +59,20 @@
     name: 'generator',
     data () {
       return {
-        targetPath: ''
+        targetPath: '',
+        packageName: '',
+        moduleName: '',
+        controllerPackageName: '',
+        servicePackageName: '',
+        modelPackageName: '',
+        daoPackageName: '',
+        eachPackage: false
       }
     },
     methods: {
+      fnGenerate () {
+        console.log(this);
+      },
       fnSelectDirectory () {
         const { dialog } = require('electron').remote;
         let options = {
@@ -68,21 +82,52 @@
           properties: ['openDirectory']
         };
         dialog.showOpenDialog(options, (path) => {
-          if(path && path.length > 0) {
+          if (path && path.length > 0) {
             this.targetPath = path[0];
+            this.fnSetPackageName();
             ipcRenderer.send('set_targetPath', this.targetPath);
+          }
+        });
+      },
+      fnSetPackageName () {
+        if (this.targetPath && this.targetPath.length < 1) {
+          return;
+        }
+
+        let packageNames = [];
+        const fs = require('fs');
+        fs.readdir(this.targetPath, (error, dirs) => {
+          if (dirs.indexOf('java') > -1) {
+            fs.readdir(`${this.targetPath}\\java`, (error, dirs) => {
+              if(dirs && dirs.length > 0) {
+                packageNames.push(dirs[0]);
+                fs.readdir(`${this.targetPath}\\java\\${packageNames[0]}`, (error, dirs) => {
+                  if(dirs && dirs.length > 0) {
+                    packageNames.push(dirs[0]);
+                    fs.readdir(`${this.targetPath}\\java\\${packageNames[0]}\\${packageNames[1]}`, (error, dirs) => {
+                      if(dirs && dirs.length > 0) {
+                        packageNames.push(dirs[0]);
+                        this.packageName = packageNames.join('.');
+                      }
+                    });
+                  }
+                });
+              }
+            })
           }
         });
       }
     },
     mounted () {
       this.targetPath = ipcRenderer.sendSync('get_targetPath');
+      this.fnSetPackageName();
     }
   }
 </script>
 
 <style>
 div.left_container {float: left; width: 50%; height: 100%; border-right: 1px solid #ccc;}
+div.left_container div.input_form {height: 100%; overflow: auto;}
 li.input_item {width: 100%; margin-top: 20px; position: relative; padding: 5px 10px 5px 0;}
 li.input_item .btn_folder {position: absolute; top: -15px; right: 10px;}
 li.input_item label.input_label {width: 100%; display: block; font-weight: 600;}
